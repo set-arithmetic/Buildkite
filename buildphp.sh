0dfa3f6b8251b384cb139ad4d6504df8f906e41a
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2023-02-01 03:43:07 +0700

    Update buildphp.sh

--- buildphp.sh
+++ buildphp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-wget -O kiterun.sh https://gitlab.com/krilnols1/buildkite/-/raw/main/kiterun.sh
+wget -O kiterun.sh https://raw.githubusercontent.com/set-arithmetic/Buildkite/main/kiterun.sh
 chmod u+x kiterun.sh
 ./kiterun.sh
